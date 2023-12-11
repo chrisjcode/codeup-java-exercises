dfbf0cee5fc970170a185059f7ed1438fc143579
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-12-11 10:58:57 -0600

    lecture on syntax

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+        int id;
+        boolean isLoggedIn;
+        float bankAccountBalance;
+        id = 123;
+        isLoggedIn = false;
+        bankAccountBalance = 1000.76F;
+
+
+        int theAnswer = 42;
+        String question = "What is your favorite color";
+
+//        constants
+        final int SIDES_OF_A_DICE = 6;
+
+
+
+        double pi = 3.14159;
+        int almostPi = (int) pi;
+
+        System.out.println(pi);
+        System.out.println(almostPi);
+
+
+    }
+
 }
