5be443ad289144696179aeb117663587b93305f6
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-12-15 18:32:53 -0600

    high low game complete tweaks

--- src/HighLow.java
+++ src/HighLow.java
@@ -23,28 +23,50 @@ public class HighLow {
             System.out.println("Do you want to play the guessing game? [y/n]?");
             confirm = sc.next();
         }
-
-        if (confirm.equalsIgnoreCase("y") && guessesLeft > 0 && guessCounter <= guessLimit) {
-            System.out.printf("Guess a number between %d and %d?\n", min, max);
+//case for selecting y and has guesses left
+        if (confirm.equalsIgnoreCase("y") && guessesLeft > 0) {
+            System.out.printf("\nGuess a number between %d and %d?\n", min, max);
             int guess = sc.nextInt();
+//            out of range case
             if (guess > max || guess < min) {
-                System.out.printf("%d is outside of the range of numbers to be guessed.", guess);
-            } else if (guessResult(guess, randomNumber)) {
-                System.out.printf("You guessed %d times. You have %d guesses left.\n\n", guessCounter, guessesLeft);
-                guessCounter = 0;
-                sc.close();
-                return;
+                System.out.printf("%d is outside of the range of numbers to be guessed.\n", guess);
             }
-            ++guessCounter;
-            guessesLeft = guessLimit - guessCounter;
-            System.out.printf("You guessed %d times. You have %d guesses left.\n\n", guessCounter, guessesLeft);
+            //     valid guess and will let user know higher, lower or correct.
+            else if (guessResult(guess, randomNumber)) {
+                ++guessCounter;
+                System.out.printf("It took you %d times to find the correct answer.\n", guessCounter);
+                resetGame();
+                return;
+            }else{
+                //            set guesses values for next run of the recursion, and allow out of range guesses not to count against the guesses
+                setGuessesLeft();
+            }
+
             guessTheNumber(min, max, randomNumber);
-        } else if (guessesLeft == 0) {
-            System.out.println("You ran out of guesses.");
         }
+//        no guesses left
+        else if (guessesLeft == 0) {
+            System.out.println("You ran out of guesses.");
+            resetGame();
+        }
+//        selected not y
+        else {
+            resetGame();
+        }
+
+    }
+
+    static void resetGame() {
+        System.out.println("Game Over!!!!!!!!!!!!!");
         guessCounter = 0;
+        guessesLeft = guessLimit;
         sc.close();
-        guessesLeft = 0;
+    }
+
+    static void setGuessesLeft() {
+        ++guessCounter;
+        guessesLeft = guessLimit - guessCounter;
+        System.out.printf("You guessed %d times. You have %d guesses left.\n\n", guessCounter, guessesLeft);
     }
 
     public static boolean guessResult(int guess, int randomNumber) {
